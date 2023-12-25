4966723d02888ca173e1165b0063f9ecc5a32183
AlbertXylonWang <[Albertxylonwang@gmail.com]>
2023-12-25 02:39:06 -0600

    attempt to add script injection on installed to existing tabs

--- scripts/background.js
+++ scripts/background.js
@@ -9,4 +9,15 @@ chrome.tabs.onUpdated.addListener((tabId, tab) => {
       });
     }
   });
+
+  chrome.runtime.onInstalled.addListener(async () => {
+  for (const cs of chrome.runtime.getManifest().content_scripts) {
+    for (const tab of await chrome.tabs.query({url: cs.matches})) {
+      chrome.scripting.executeScript({
+        target: {tabId: tab.id},
+        files: cs.js,
+      });
+    }
+  }
+});
   
